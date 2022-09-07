a1de6182b54c6f800afac1baa5a867406ee416d7
mohan <sahumohan338@gmail.com>
2022-09-07 16:07:07 +0530

    [mohan]add: Add Data In AddressBook Linq

--- AddressBook/AddressBook/Book.cs
+++ AddressBook/AddressBook/Book.cs
@@ -61,5 +61,27 @@ namespace AddressBook
             dataTable.Columns.Add(dtCol);
 
         }
+        public void InsertionInDataTable(Contact contact)
+        {
+            DataRow dtRow = dataTable.NewRow();
+            dtRow["Id"] = contact.Id;
+            dtRow["FirstName"] = contact.FirstName;
+            dtRow["LastName"] = contact.LastName;
+            dtRow["Address"] = contact.Address;
+            dtRow["City"] = contact.City;
+            dtRow["State"] = contact.State;
+            dtRow["Zip"] = contact.zip;
+            dtRow["PhoneNumber"] = contact.PhoneNumber;
+            dtRow["Email"] = contact.Email;
+            dataTable.Rows.Add(dtRow);
+
+        }
+        public void Display()
+        {
+            foreach (DataRow dtRows in dataTable.Rows)
+            {
+                Console.WriteLine(" Id: {0} \n First Name: {1} \n Last Name: {2} \n Address: {3} \n City: {4} \n State: {5} \n Zip: {6} \n Phone Number: {7} \n Email: {8} \n", dtRows["ID"], dtRows["FirstName"], dtRows["LastName"], dtRows["Address"], dtRows["City"], dtRows["State"], dtRows["Zip"], dtRows["PhoneNumber"], dtRows["Email"]);
+            }
+        }
     }
 }
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -5,7 +5,23 @@
         public static void Main(string[] args)
         {
             Book addressBook = new Book();
+            Contact contact1 = new Contact();
+
             addressBook.CreateTableUsingLinq();
+            contact1.Id = 1;
+            contact1.FirstName = "Mohan";
+            contact1.LastName = "Sahu";
+            contact1.PhoneNumber = 7898524871;
+            contact1.Email = "mohan54@gmail.com";
+            contact1.Address = "katulboard";
+            contact1.City = "Bhilai";
+            contact1.State = "CG";
+            contact1.zip = 490020;
+            addressBook.InsertionInDataTable(contact1);
+
+
+
+            addressBook.Display();
         }
     }
 }
